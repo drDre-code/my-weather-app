30c6ab32131ed4085bfbd0efc1f734cfc716c365
drDre-code <ebeneezer.akinkahunsi@decagon.dev>
2021-08-14 03:37:49 +0100

    fixing build issues

--- public/script/app.js
+++ public/script/app.js
@@ -1,18 +1,18 @@
-const weatherForm = document.querySelector('form')
-const input = document.querySelector('input')
-const response1 = document.querySelector('#response1')
-const response2 = document.querySelector('#response2')
+const weatherForm = document.querySelector('form');
+const input = document.querySelector('input');
+const response1 = document.querySelector('#response1');
+const response2 = document.querySelector('#response2');
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault(); //prevents the default pattern of forms to automatically refresh
-    response1.textContent = 'loading...'
-    response2.textContent = ''
-    const address = input.value
-    fetch(`http://localhost:3000/weather?address=${address}`).then((response) => response.json()).then(data => {
+    response1.textContent = 'loading...';
+    response2.textContent = '';
+    const address = input.value;
+    fetch(`/weather?address=${address}`).then((response) => response.json()).then(data => {
         if (data.errorMessage) {
-            response1.textContent = data.errorMessage
+            response1.textContent = data.errorMessage;
         } else {
-            response1.textContent = data.location
-            response2.textContent = data.forecast
+            response1.textContent = data.location;
+            response2.textContent = data.forecast;
         }
-    })
-})
+    });
+});
